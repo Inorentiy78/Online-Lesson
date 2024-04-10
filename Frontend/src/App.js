defc17d87acf1d6196c42d76118c28a8d7046b8c
sayat2006S <148536637+sayat2006S@users.noreply.github.com>
2024-04-10 17:50:30 +0500

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import DisRegister from './DisRegistr.js'
+import './App.css' 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <DisRegister/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
