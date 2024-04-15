e89eb28d38ca0081cb80ea4c7692994e22b2c5ae
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2024-04-15 18:39:43 +0500

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import DisRegistor from './register/DisRegistr'
+import './App.css' 
+import Desing from './register/Desing'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DisRegistor/> 
+       <Desing/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
