629759b4d79020d1b39b4a2f4e13a2380b429669
Markokakola <148535019+markokakola@users.noreply.github.com>
2024-04-16 22:14:51 +0500

    Add files via upload

--- Frontend/src/register/Contacts.js
+++ Frontend/src/register/Contacts.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import './Contacts.css'; // Подключаем файл стилей
+import PhoneIcon from '@mui/icons-material/Phone';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TelegramIcon from '@mui/icons-material/Telegram';
 
 function Contacts() {
     return (
@@ -15,17 +18,20 @@ function Contacts() {
             </div>
             <div className='Zogolovok'>Связаться с нами: </div>
             <div className="rectangle "></div>
+            <MailOutlineIcon className="icon1" fontSize="large" />
             <div className='h1'>
                 Наша почта:
             </div>
             <div className='h2'>
                 onlinelessons@gmail.com
-                
             </div>
+            <PhoneIcon className="icon2" fontSize="large"/>
             <div className='h3'>Номер администрации: </div>
             <div className='h4'> +7-(***)-***-**-** </div>
+            <WhatsAppIcon className="icon3" fontSize="large"/>
             <div className='h5'>WhatsApp: </div>
             <div className='h6'> +7-(***)-***-**-** </div>
+            <TelegramIcon className="icon4" fontSize="large"/>
             <div className='h7'>Telegram: </div>
             <div className='h8'>@onlinelessons</div>
         </div>
