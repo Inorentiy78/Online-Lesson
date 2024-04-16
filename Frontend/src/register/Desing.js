35c610e7f6a087216d0ea4547c0a373cb76ebd23
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2024-04-16 14:14:16 +0500

    Update Desing.js

--- Frontend/src/register/Desing.js
+++ Frontend/src/register/Desing.js
@@ -14,25 +14,26 @@ function Desing() {
                     <div className="english-language">Английский язык</div>
                     <div className="spanish-language">Испанский язык</div>
                     <div className="italian-language">Итальянский язык</div>
-                    <div className="kazakh-language">Казахский язык</div>
+                    {/* <div className="kazakh-language">Казахский язык</div>
                     <div className="chinese-language">Китайский язык</div>
-                    <div className="russian-language">Русский язык</div>
+                    <div className="russian-language">Русский язык</div> */}
                 </div>
                 
-    <input type="text" className="search-input" placeholder="Введите запрос..." />
-
+    <input type="text" className="search-input" placeholder="Введите название курса..." />
+    
+ 
                 <div className="line"></div>
                 
-                <div className="search-button"></div>
+                <div className="search-button">  Найти</div>
                 <div className="search-string"></div>
             </div>
             <div className="card-container">
-                <div className="english-card"></div>
-                <div className="spanish-card"></div>
-                <div className="italian-card"></div>
-                <div className="kazakh-card"></div>
-                <div className="chinese-card"></div>
-                <div className="russian-card"></div>
+                <div className="english-card">Английский язык</div>
+                <div className="spanish-card">Испанский язык</div>
+                <div className="italian-card">Итальянский язык</div>
+                <div className="kazakh-card">Казахский язык</div>
+                <div className="chinese-card">Китайский язык</div>
+                <div className="russian-card">Русский язык</div>
             </div>
         </div>
     );
