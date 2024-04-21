97543c758ac36ca4948993dbb6ddf9dba59dfdfd
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2024-04-21 20:49:45 +0500

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,19 @@
-import React,{useState} from 'react';
-import DisRegistor from './register/DisRegistr';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DisRegistr from './register/DisRegistr';
 import Home1 from './register/Home1';
 import OKyrsah from './register/OKyrsah';
 import Contacts from './register/Contacts';
 import Podrobnee from './register/Podrobnee';
 import Zapisatsa from './register/Zapisatsa';
 import Home2 from './register/Home2';
-
+import Test from './register/Test';
+import Video from './register/Video';
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [view, setView] = useState('initial'); // initial or login
 
   const openModal = (language) => {
     setSelectedLanguage(language);
@@ -21,16 +24,28 @@ function App() {
     setSelectedLanguage('');
     setModalOpen(false);
   };
+
+  const handleLoginClick = () => { // Функция для кнопки Войти
+    setView('login');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <DisRegistor/>  
         <Podrobnee isOpen={modalOpen} onClose={closeModal} language={selectedLanguage} />
-        <Home1 openModal={openModal} />
-       {/*<OKyrsah/>
-       <Contacts/>
-       <Zapisatsa/>
-       <Home2/>*/}
+         <Router>
+          <Routes>
+            <Route exact path="/" element={<Home2 />} />
+            <Route path="/about-courses" element={<OKyrsah />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </Router> 
+        {/*<Contacts/>*/}
+        {/* <DisRegistr handleLoginClick={handleLoginClick} />  
+        <Home1 openModal={openModal} onLoginClick={handleLoginClick} />
+      <Zapisatsa/>
+      {/*<Test/>
+       <Video/> */}
       </header>
     </div>
   );
